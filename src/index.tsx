8d9f8490bba5e6c08e136125d5f0329f4529e187
David <davidferreira.m1@outlook.com>
2021-03-21 16:01:33 -0300

    cssbaseline material-ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import reportWebVitals from './reportWebVitals';
+
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
