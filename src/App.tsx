36d59732e3f9106d7ba15e801c59664861198d7e
David <davidferreira.m1@outlook.com>
2021-03-21 16:02:16 -0300

    renderização do background

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { Background1 } from './shared';
+
 function App() {
-  return <div />;
+  return (
+    <Background1>
+      <div />
+    </Background1>
+  );
 }
 export default App;
