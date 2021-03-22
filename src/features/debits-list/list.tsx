2cb30137e356be99c28f14217bde12b08ca0e42c
David <davidferreira.m1@outlook.com>
2021-03-22 01:20:26 -0300

    moda de confirmação de exclusão criado

--- src/features/debits-list/list.tsx
+++ src/features/debits-list/list.tsx
@@ -7,22 +7,38 @@ import {
   ListItemSecondaryAction,
   ListItemText,
 } from '@material-ui/core';
+
 import LocalAtmIcon from '@material-ui/icons/LocalAtm';
 import DeleteIcon from '@material-ui/icons/Delete';
 
 import { useBoxTransition } from './box-transition';
+import { ItemListDebitProps } from './interfaces';
 
-export function ItemList() {
+import { DialogRefProps } from '../../components/modal-dialog/interface';
+import { ModalDialog } from '../../components';
+
+export function ItemList(props: ItemListDebitProps) {
   const { spread } = useBoxTransition();
+  const { id, username, debitValue, openModal } = props;
+
+  const onConfirm = () => {
+    openModal(
+      'Excluir Dívida',
+      `Confirmar exclusão da dívida de ${username.toUpperCase()} no valor de ${debitValue}?`,
+      () => {
+        // console.log('');
+      },
+    );
+  };
 
   return (
-    <ListItem component="li" button onClick={spread}>
+    <ListItem component="li" button onClick={() => spread(id)}>
       <ListItemIcon>
         <LocalAtmIcon />
       </ListItemIcon>
-      <ListItemText primary="Fulano de tal" secondary="R$ 3.000,00" />
+      <ListItemText primary={username} secondary={debitValue} />
       <ListItemSecondaryAction>
-        <IconButton>
+        <IconButton onClick={onConfirm}>
           <DeleteIcon />
         </IconButton>
       </ListItemSecondaryAction>
@@ -31,12 +47,45 @@ export function ItemList() {
 }
 
 export function List() {
+  const modal = React.createRef<DialogRefProps>();
+  const openModal = (title: string, text: string, onConfirm: () => void) => {
+    modal.current?.open({
+      title,
+      text,
+      onConfirm,
+    });
+  };
   return (
-    <MuiList component="ul">
-      <ItemList />
-      <ItemList />
-      <ItemList />
-      <ItemList />
-    </MuiList>
+    <>
+      <MuiList component="ul">
+        <ItemList
+          id="0"
+          username="Fulano de tal"
+          debitValue="R$ 3.000,00"
+          openModal={openModal}
+        />
+        <ItemList
+          id="1"
+          username="Fulano de tal"
+          debitValue="R$ 3.000,00"
+          openModal={openModal}
+        />
+        <ItemList
+          id="2"
+          username="Fulano de tal"
+          debitValue="R$ 3.000,00"
+          openModal={openModal}
+        />
+        <ItemList
+          id="3"
+          username="Fulano de tal"
+          debitValue="R$ 3.000,00"
+          openModal={openModal}
+        />
+      </MuiList>
+      <>
+        <ModalDialog ref={modal} />
+      </>
+    </>
   );
 }
