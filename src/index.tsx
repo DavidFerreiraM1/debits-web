a5fbd4f68b97a85717d696382bbf9fa8056ec2a5
David <davidferreira.m1@outlook.com>
2021-03-22 01:18:52 -0300

    fonte e tema adicionados

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core';
 import reportWebVitals from './reportWebVitals';
 
+import { themeDefault } from './styles/theme';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <CssBaseline />
-    <App />
+    <ThemeProvider theme={themeDefault}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
---
+++ src/styles/theme.ts
@@ -0,0 +1,7 @@
+import { createMuiTheme } from '@material-ui/core';
+
+export const themeDefault = createMuiTheme({
+  typography: {
+    fontFamily: ["'Roboto', sans-serif"].join(','),
+  },
+});
