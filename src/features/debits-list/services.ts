c49b5d523e6af045059de77f1273988f5109edb0
David <davidferreira.m1@outlook.com>
2021-03-23 03:18:26 -0300

    serviços criados

---
+++ src/features/debits-list/services.ts
@@ -0,0 +1,29 @@
+import { IClientUser, IDebit } from '../../core/interfaces';
+import { httpResponse } from '../../http-client/http-response';
+import {
+  DebitClientApi,
+  JsonPlaceholderClientApi,
+} from '../../http-client/instances';
+import { IHttpResponse } from '../../http-client/interfaces';
+
+export async function getAllDebitsService(): Promise<
+  IHttpResponse<IDebit[] | null>
+> {
+  try {
+    const { data, status } = await DebitClientApi.get('');
+    return httpResponse<IDebit[]>(true, status, data.result, null);
+  } catch (err) {
+    return httpResponse<null>(false, 404, null, err);
+  }
+}
+
+export async function getUsersService(): Promise<
+  IHttpResponse<IClientUser[] | null>
+> {
+  try {
+    const { data, status } = await JsonPlaceholderClientApi.get('/users');
+    return httpResponse<IClientUser[]>(true, status, data, null);
+  } catch (err) {
+    return httpResponse<null>(false, 404, null, err);
+  }
+}
