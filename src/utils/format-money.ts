b015259204a42f0a38e4b18a0023dbb78a49bf53
David <davidferreira.m1@outlook.com>
2021-03-23 00:58:02 -0300

    utilitário para formatação de dados monetários

---
+++ src/utils/format-money.ts
@@ -0,0 +1,17 @@
+import Dinero from 'dinero.js';
+
+export function formatMoney(value: string) {
+  let val = '';
+
+  if (!value) {
+    val = '0';
+  }
+
+  val = value.trim().replace('R$', '').replaceAll('.', '').replaceAll(',', '');
+
+  const treatedValue = Dinero({ amount: parseFloat(val), currency: 'BRL' })
+    .setLocale('pt-BR')
+    .toFormat();
+
+  return treatedValue;
+}
