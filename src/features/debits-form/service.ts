dbe96e4c9b792c3667444aa7f016558253f04c2d
David <davidferreira.m1@outlook.com>
2021-03-23 15:12:42 -0300

    metodos de busca por id e atualização de dados criados

--- src/features/debits-form/service.ts
+++ src/features/debits-form/service.ts
@@ -6,6 +6,8 @@ import {
 } from '../../http-client/instances';
 import { IHttpResponse } from '../../http-client/interfaces';
 
+const uuid = `?uuid=${process.env.REACT_APP_UUID}`;
+
 export async function getUsersService(): Promise<
   IHttpResponse<IClientUser[] | null>
 > {
@@ -17,11 +19,34 @@ export async function getUsersService(): Promise<
   }
 }
 
+export async function getDebitByIdService(
+  id: string,
+): Promise<IHttpResponse<IDebit | null>> {
+  try {
+    const { status, data } = await DebitClientApi.get(`/${id}/${uuid}`);
+    return httpResponse<IDebit>(true, status, data.result, null);
+  } catch (err) {
+    return httpResponse<null>(false, 404, null, err);
+  }
+}
+
 export async function postDebitService(
   body: IDebit,
 ): Promise<IHttpResponse<null>> {
   try {
-    const { status } = await DebitClientApi.post('', body);
+    const { status } = await DebitClientApi.post(`${uuid}`, body);
+    return httpResponse<null>(true, status, null, null);
+  } catch (err) {
+    return httpResponse<null>(false, 404, null, err);
+  }
+}
+
+export async function putDebitService(
+  id: string,
+  body: IDebit,
+): Promise<IHttpResponse<null>> {
+  try {
+    const { status } = await DebitClientApi.put(`/${id}/${uuid}`, body);
     return httpResponse<null>(true, status, null, null);
   } catch (err) {
     return httpResponse<null>(false, 404, null, err);
--- src/features/debits-list/services.ts
+++ src/features/debits-list/services.ts
@@ -6,11 +6,13 @@ import {
 } from '../../http-client/instances';
 import { IHttpResponse } from '../../http-client/interfaces';
 
+const uuid = `?uuid=${process.env.REACT_APP_UUID}`;
+
 export async function getAllDebitsService(): Promise<
   IHttpResponse<IDebit[] | null>
 > {
   try {
-    const { data, status } = await DebitClientApi.get('');
+    const { data, status } = await DebitClientApi.get(`/${uuid}`);
     return httpResponse<IDebit[]>(true, status, data.result, null);
   } catch (err) {
     return httpResponse<null>(false, 404, null, err);
