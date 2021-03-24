e321ad3d78f9c0a9fd4a3eba2f641c2f75072827
David <davidferreira.m1@outlook.com>
2021-03-23 22:03:59 -0300

    estilização para scroll

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,19 @@ import { Container } from '@material-ui/core';
 
 import { Background1 } from './shared';
 import { DebitsList } from './features';
+import { AlertContextProvider } from './components/alert';
+
+import './scroll.css';
 
 function App() {
   return (
-    <Background1>
-      <Container maxWidth="lg">
-        <DebitsList />
-      </Container>
-    </Background1>
+    <AlertContextProvider>
+      <Background1>
+        <Container maxWidth="lg">
+          <DebitsList />
+        </Container>
+      </Background1>
+    </AlertContextProvider>
   );
 }
 export default App;
