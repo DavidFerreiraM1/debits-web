e819dc3334c3b7447e0393e0a0789deb21e08b09
David <davidferreira.m1@outlook.com>
2021-03-23 22:04:10 -0300

    alerta criado

---
+++ src/components/alert/styles.ts
@@ -0,0 +1,11 @@
+import { makeStyles } from '@material-ui/core';
+
+export const styles = makeStyles({
+  alert: {
+    zIndex: 30000,
+    position: 'fixed',
+    top: 0,
+    right: 0,
+    margin: 24,
+  },
+});
